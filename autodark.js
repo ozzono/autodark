7f1515cbdd5d5c7e254152b512c929d7f8dac7e4
Hugo Virgílio <huvirgilio@gmail.com>
2020-06-26 14:55:41 -0300

    Added dynamic await interval

--- autodark.js
+++ autodark.js
@@ -1,1 +1,7 @@
-setTimeout(() => {document.querySelectorAll('body')[0].className += ' dark';void(0); }, 2000)
+const sleep = setInterval(()=>{
+    const body=document.querySelectorAll("web");
+    if (body){
+        document.querySelectorAll('body')[0].className += ' dark';
+        void(0);
+    }
+1000})
