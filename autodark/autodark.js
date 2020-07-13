fd18361bc97441e691a88c66f8c079eb08404ca1
Hugo Virgílio <huvirgilio@gmail.com>
2020-07-13 18:03:07 -0300

    Fixed minor .git folder misplacement

--- autodark/autodark.js
+++
@@ -1,7 +0,0 @@
-var body=document.querySelectorAll("web");
-while(!body){
-    setTimeout(()=>{
-        body=document.querySelectorAll("web");
-    }, 1000);
-}
-document.querySelectorAll('body')[0].className += ' dark';
